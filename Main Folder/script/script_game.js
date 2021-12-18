a7ccb45e3516bfa911aadc697517359d856055b4
Gekrakel <Gekrakel@users.noreply.github.com>
2021-12-18 22:39:29 +0100

    Simplified board creation
    -Simplified array creation
    -Added event listener for keydown
    -Moved from a [column][row] structure to [row][column]

--- Main Folder/script/script_game.js
+++ Main Folder/script/script_game.js
@@ -5,100 +5,131 @@ var spielerGridColumn = 4;
 var schluessel = document.getElementById("keyOnGamePage");
 var keyCollected = false;
 
-//spielfeldArray[spalte][zeile] --> 0 = frei, 1 = wand, 2 = schluessel, 3 = tuer
-var spielfeldArray = [9];
-for (let i = 0; i < 9; i++) {
-	spielfeldArray[i] = [10];
-	for (let j = 0; j < 10; j++) {
-		switch(i) {
-			case 0:
-				spielfeldArray[i][j] = 1;
-				break;
-			case 1:	
-			case 2:
-				if (j == 0 || j == 9) {
-					spielfeldArray[i][j] = 1;
-				} else {
-					spielfeldArray[i][j] = 0;
-				}
-				break;
-			case 3:
-				if (j == 0 || j == 4 || j == 8) {
-					spielfeldArray[i][j] = 1;
-				} else {
-					spielfeldArray[i][j] = 0;
-				}
-				break;
-			case 4:
-				if (j == 0) {
-					spielfeldArray[i][j] = 3;
-					break;
-				} 
-				if (j == 1 || j == 2 || j == 3) {
-					spielfeldArray[i][j] = 0;
-				} else {
-					spielfeldArray[i][j] = 1;
-				}
-				break;
-			case 5:
-				if (j == 8) {
-					spielfeldArray[i][j] = 2;
-					break;
-				}
-				if (j == 0 || j == 4 || j == 8) {
-					spielfeldArray[i][j] = 1;
-				} else {
-					spielfeldArray[i][j] = 0;
-				}
-				break;
-			case 6:
-			case 7:
-				if (j == 0 || j == 9) {
-					spielfeldArray[i][j] = 1;
-				} else {
-					spielfeldArray[i][j] = 0;
-				}
-				break;
-			case 8:
-				spielfeldArray[i][j] = 1;
-				break;
-		}
+function onKeyPressed(e){
+	if(e.code=='KeyW' || e.code=='ArrowUp'){
+		movePlayerUp();
+	}else if(e.code=='KeyS' || e.code=='ArrowDown'){
+		movePlayerDown();
+	}else if(e.code=='KeyA' || e.code=='ArrowLeft'){
+		movePlayerLeft();
+	}else if(e.code=='KeyD' || e.code=='ArrowRight'){
+		movePlayerRight();
 	}
 }
+
+// Register events
+document.addEventListener("keydown", onKeyPressed);
+
+
+//spielfeldArray[zeile][spalte] --> 0 = frei, 1 = wand, 2 = schluessel, 3 = tuer
+var spielfeldArray=[
+	[1,1,1,1,3,1,1,1,1],
+	[1,0,0,0,0,0,0,0,1],
+	[1,0,0,0,0,0,0,0,1],
+	[1,0,0,0,0,0,0,0,1],
+	[1,0,0,1,1,1,0,0,1],
+	[1,0,0,0,1,0,0,0,1],
+	[1,0,0,0,1,0,0,0,1],
+	[1,0,0,0,1,0,0,0,1],
+	[1,0,0,0,1,2,0,0,1],
+	[1,1,1,1,1,1,1,1,1]
+];
+
+// var spielfeldArray = [9];
+// for (let i = 0; i < 9; i++) {
+// 	spielfeldArray[i] = [10];
+// 	for (let j = 0; j < 10; j++) {
+// 		switch(i) {
+// 			case 0:
+// 				spielfeldArray[j][i] = 1;
+// 				break;
+// 			case 1:	
+// 			case 2:
+// 				if (j == 0 || j == 9) {
+// 					spielfeldArray[j][i] = 1;
+// 				} else {
+// 					spielfeldArray[j][i] = 0;
+// 				}
+// 				break;
+// 			case 3:
+// 				if (j == 0 || j == 4 || j == 8) {
+// 					spielfeldArray[j][i] = 1;
+// 				} else {
+// 					spielfeldArray[j][i] = 0;
+// 				}
+// 				break;
+// 			case 4:
+// 				if (j == 0) {
+// 					spielfeldArray[j][i] = 3;
+// 					break;
+// 				} 
+// 				if (j == 1 || j == 2 || j == 3) {
+// 					spielfeldArray[j][i] = 0;
+// 				} else {
+// 					spielfeldArray[j][i] = 1;
+// 				}
+// 				break;
+// 			case 5:
+// 				if (j == 8) {
+// 					spielfeldArray[j][i] = 2;
+// 					break;
+// 				}
+// 				if (j == 0 || j == 4 || j == 8) {
+// 					spielfeldArray[j][i] = 1;
+// 				} else {
+// 					spielfeldArray[j][i] = 0;
+// 				}
+// 				break;
+// 			case 6:
+// 			case 7:
+// 				if (j == 0 || j == 9) {
+// 					spielfeldArray[j][i] = 1;
+// 				} else {
+// 					spielfeldArray[j][i] = 0;
+// 				}
+// 				break;
+// 			case 8:
+// 				spielfeldArray[j][i] = 1;
+// 				break;
+// 		}
+// 	}
+// }
 
 function isMoveValid(direction) {
 	switch(direction) {
 		case "up":
-			if (spielfeldArray[spielerGridColumn - 1][spielerGridRow - 1 - 1] == 3) {
-				spieler.remove();
-				return true;
+			if (spielfeldArray[spielerGridRow - 1 - 1][spielerGridColumn - 1] == 3) {
+				if(keyCollected){
+					spieler.remove();
+					return true;
+				}
 			}
-			if (spielfeldArray[spielerGridColumn - 1][spielerGridRow - 1 - 1] == 0) {
+			if (spielfeldArray[spielerGridRow - 1 - 1][spielerGridColumn - 1] == 0) {
 				return true;
 			}
 			return false;
 		case "down":
-			if (spielfeldArray[spielerGridColumn - 1][spielerGridRow - 1 + 1] == 2) {
+			if (spielfeldArray[spielerGridRow - 1 + 1][spielerGridColumn - 1] == 2) {
 				keyCollected = true;
 				schluessel.remove();
 				return true;
 			}
-			if (spielfeldArray[spielerGridColumn - 1][spielerGridRow - 1 + 1] == 0) {
+			if (spielfeldArray[spielerGridRow - 1 + 1][spielerGridColumn - 1] == 0) {
 				return true;
 			}
 			return false;
 		case "left":
-			if (spielfeldArray[spielerGridColumn - 1 - 1][spielerGridRow - 1] == 2) {
+			if (spielfeldArray[spielerGridRow - 1][spielerGridColumn - 1 - 1] == 2) {
 				keyCollected = true;
 				schluessel.remove();
 				return true;
 			}
-			if (spielfeldArray[spielerGridColumn - 1 - 1][spielerGridRow - 1] == 0) {
+			if (spielfeldArray[spielerGridRow - 1][spielerGridColumn - 1 - 1] == 0) {
 				return true;
 			}
 			return false;
 		case "right":
-			if (spielfeldArray[spielerGridColumn - 1 + 1][spielerGridRow - 1] == 0) {
+			if (spielfeldArray[spielerGridRow - 1][spielerGridColumn - 1 + 1] == 0) {
 				return true;
 			}
 			return false;
@@ -132,15 +163,4 @@ function movePlayerRight() {
 		spieler.setAttribute("style", "grid-column:" + spielerGridColumn + "/" + (spielerGridColumn + 1) + "; grid-row: " + spielerGridRow + "/" + (spielerGridRow + 1) + ";");
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
 
